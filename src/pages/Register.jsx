dae1426c407132947bb97717303001b0cba60f8e
Soonnick0123 <soonnick02@gmail.com>
2024-03-27 21:23:22 +0800

    add button

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -31,7 +31,49 @@ export default function Register() {
         return (
             <>
                 <style jsx>
-                    {``}
+                    {`
+                        .btn-1 {
+                            background: #0F0F6D;
+                            color: #ffffff;
+                            font-family: 'Poppins';
+                            font-weight: bold;
+                            cursor: pointer;
+                            font-size: 1em;
+                            padding: 1rem;
+                            border: 0;
+                            transition: all 0.5s;
+                            border-radius: 10px;
+                            width: auto;
+                            position: relative;
+                            user-select: none;
+                            animation: thirdFadeIn 2s forwards;
+                            animation-delay: 4.2s;
+
+                            span {
+                                position: absolute;
+                                left: 75%;
+                                top: 27%;
+                                right: 5%;
+                                bottom: 0;
+                                opacity: 0;
+                            }
+
+                            &:hover {
+                                background: #2b2bff;
+                                transition: all 0.5s;
+                                border-radius: 10px;
+                                box-shadow: 0px 6px 15px #0000ff61;
+                                padding: 1rem 3.5rem 1rem 1rem;
+
+                                span {
+                                    opacity: 1;
+                                    transition: all 0.5s;
+                                }
+                            }
+
+
+                        }
+                    `}
                 </style>
 
                 {/* <Modal isOpen={selectMembershipModal} toggle={()=> setSelectMembershipModal(false)}>
@@ -54,6 +96,24 @@ export default function Register() {
                     </div>
                 </Modal> */}
 
+                <label for="modal" class="modal-bg"></label>
+
+                <div class="modal-content">
+                    <label for="modal" class="close">
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                    </label>
+                    <header>
+                        <h2>So This is a Modal</h2>
+                    </header>
+                    <article class="content">
+                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.</p>
+                    </article>
+                    <footer>
+                        <a href="https://joshuaward.me" target="_blank" class="button success">Accept</a>
+                        <label for="modal" class="button danger">Decline</label>
+                    </footer>
+                </div>
+
                 {loading && <LoadingScreen zIndex={2}/>}
 
                 <div className='container'>
@@ -62,7 +122,10 @@ export default function Register() {
                 <div className='second-container'>
 
                     <div style={{width:"100%",height:"85%",margin:"4%",background:"white",borderRadius:"20px",display:"flex",flexDirection:"column",padding:25,gap:25}}>
-                        <div style={{fontWeight:"bold",fontSize:"larger",paddingLeft:"10px"}}>Registered Customer</div>
+                        <div style={{width:"100%",display:"flex",flexDirection:"row",justifyContent:"space-between",alignItems:"center"}}>
+                            <div style={{fontWeight:"bold",fontSize:"larger",paddingLeft:"10px"}}>Registered Customer</div>
+                            <div className="btn-1">Register<span className="material-symbols-outlined">person_add</span></div>
+                        </div>
 
                         <div style={{width:"100%",height:"90%",display:"grid",overflowY:"auto",padding:10,gridTemplateColumns: "30% 30% 30%",gridTemplateRows: "20% 20% 20%",gridColumnGap:"4%",gridRowGap:"5%"}}>
 
